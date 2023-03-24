58d0171a2f4750ef3d857de18d0e41f545c7b8da
umeed-salman <89387431+umeed-salman@users.noreply.github.com>
2023-03-24 20:56:34 +0500

    Update server.py

--- server.py
+++ server.py
@@ -5,10 +5,14 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 
+
 app = Flask(__name__)
 
-# Replace YOUR_API_KEY with your Alpha Vantage API key
-ts = TimeSeries(key='WO6IIEBNMUTO92UN', output_format='pandas')
+
+# Replace YOUR_API_KEY with
+# your Alpha Vantage API key
+ts = TimeSeries(key='WO6IIEBNMUTO92UN',
+                output_format='pandas')
 
 # Load the LSTM model
 model = tf.keras.models.load_model('model.h5')
@@ -24,12 +28,14 @@ def create_dataset(data, look_back=1):
         Y.append(data[i+look_back, 0])
     return np.array(X), np.array(Y)
 
+
 # Home page route
 @app.route('/')
 def home():
     # Retrieve the stock data
     symbol='IBM'
-    data, meta_data = ts.get_daily_adjusted(symbol=symbol, outputsize='compact')
+    data, meta_data = ts.get_daily_adjusted(
+        symbol=symbol, outputsize='compact')
     dates=[]
     for i in data.index:
         dates.append(i.strftime("%m/%d/%Y"))
@@ -51,24 +57,31 @@ def home():
     predicted = model.predict(X)
     predicted = scaler.inverse_transform(predicted)
 
-    # Get the last 7 days of actual stock prices and the predicted price for the next day
-    #last_7_days = scaler.inverse_transform(data[-7:])
+    # Get the last 7 days of actual stock prices
+    # and the predicted price for the next day
+    # last_7_days = scaler.inverse_transform(data[-7:])
 
     last_7_days = []
     for i in range(-7, 0):
-        last_7_days.append([dates[i], scaler.inverse_transform([data[i]])[0][0]])
+        last_7_days.append([dates[i], 
+                            scaler.inverse_transform([data[i]])[0][0]])
 
     next_day_predicted = predicted[-1][0]
 
     # Render the template and pass the data to it
-    return render_template('home.html', symbol=symbol, last_7_days=last_7_days, next_day_predicted=next_day_predicted)
+    return render_template('home.html', symbol=symbol,
+                           last_7_days=last_7_days,
+                           next_day_predicted=next_day_predicted)
+
 
 # Prediction page route
 @app.route('/prediction', methods=['GET', 'POST'])
 def prediction():
     if request.method == 'POST':
         # Get the user input
-        user_input = np.array(request.form.getlist('user_input'), dtype=np.float32)
+        user_input = np.array(
+            request.form.getlist('user_input'),
+            dtype=np.float32)
 
         # Normalize the user input
         user_input = scaler.transform(user_input.reshape(-1, 1))
@@ -84,10 +97,14 @@ def prediction():
         next_day_predicted = predicted[0][0]
 
         # Render the template and pass the data to it
-        return render_template('prediction.html', user_input=request.form.getlist('user_input'), next_day_predicted=next_day_predicted)
+        return render_template('prediction.html',
+                               user_input=request.form.getlist('user_input'),
+                               next_day_predicted=next_day_predicted)
 
-    # Render the template without any data if the user hasn't submitted the form yet
+    # Render the template without any data
+    # if the user hasn't submitted the form yet
     return render_template('prediction.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
